79d970a87697a456b6d58ab98312edf81aef4580
axel ridray <axel.ridray@sfr.fr>
2021-12-13 16:00:13 +0100

    new bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,15 @@
 class BookmarksController < ApplicationController
+
+  def new 
+    @bookmark = bookmark.new
+  end
+
+  def create 
+    @bookmark = bookmark.new(bookmark_params)
+    @bookmark.save
+    
+    redirect_to restaurant_lists_path #redirige vers l index si ont veut rediriger vers le produit crée on lui passe la route associer
+  end
+
+
 end
