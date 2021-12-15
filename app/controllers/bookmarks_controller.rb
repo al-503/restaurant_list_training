11b14f149b9de0f0e588cbc48e0c5fae627dae3a
axel ridray <axel.ridray@sfr.fr>
2021-12-15 14:32:21 +0100

    index resta&urant dans bookmar new

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,23 +3,25 @@ class BookmarksController < ApplicationController
   def new 
     @restaurant_list = RestaurantList.find(params[:restaurant_list_id])
     @bookmark = Bookmark.new
+    @restaurants = Restaurant.all
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @restaurant_list = RestaurantList.find(params[:restaurant_list_id])
     @bookmark.restaurant_list = @restaurant_list
+    @restaurants = Restaurant.all
     if @bookmark.save
       redirect_to restaurant_list_path(@restaurant_list)
     else
-      render :newx
+      render :new
     end
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:restaurant_id)
+    params.require(:bookmark).permit(:restaurant_ids)
   end
 
 end
